20c66278fb385969cbac17ae9312f50f8bc9bf52
geeksploit <geeksploit@shootan.com>
2017-10-04 19:37:03 +0300

    Add a menu item for the task8

--- lesson_1_homework/main.c
+++ lesson_1_homework/main.c
@@ -5,7 +5,7 @@
 
 int main() {
 
-    void (*menu[15])(void) = {&task0, &task1, &task2, &task3, &task4, &task5, &task6, &task7};
+    void (*menu[15])(void) = {&task0, &task1, &task2, &task3, &task4, &task5, &task6, &task7, &task8};
 
     int choice = -1;
     do {
@@ -23,6 +23,7 @@ int main() {
         printf("\n[%2d] %s", 5, "guess the season by month");
         printf("\n[%2d] %s", 6, "print age");
         printf("\n[%2d] %s", 7, "match chess fields by color");
+        printf("\n[%2d] %s", 8, "print squares and cubes");
         printf("\n> ");
     } while (scanf("%d", &choice));
 
