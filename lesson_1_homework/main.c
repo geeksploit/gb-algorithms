5bcd2b5d64b9475da92094a2ca53eaef566309ae
geeksploit <geeksploit@shootan.com>
2017-10-04 17:46:36 +0300

    Implement menu stub

--- lesson_1_homework/main.c
+++ lesson_1_homework/main.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 
 int main() {
+
+    int choice = -1;
+    do {
+        if (choice >= 0 && choice <= 14) {
+            printf("\nTASK %d\n", choice);
+        }
+        printf("\n\nMENU");
+        printf("\nplease enter your choice to proceed:");
+        printf("\n> ");
+    } while (scanf("%d", &choice));
+
     return 0;
 }
