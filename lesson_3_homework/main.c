c9e57ac0e113c101b87b892dc85c973630aef303
geeksploit <geeksploit@shootan.com>
2017-10-11 18:34:05 +0300

    Iterate through the head of the array without the last few elements

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -132,6 +132,8 @@ int bubbleSortOptimizedMinusICheckSort(int *array, size_t size) {
     int counter = 0;
     for (int i = 0; i < size; i++) {
         int isSorted = 1;
+        for (int j = 0; j < size - 1 - i; j++) {
+        }
     }
     return counter;
 }
