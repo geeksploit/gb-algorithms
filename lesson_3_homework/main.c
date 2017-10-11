688b9e4de61cc6290fa3e8ac188198910e7d1b49
geeksploit <geeksploit@shootan.com>
2017-10-11 18:30:16 +0300

    Print the amount of operations for the "check sorting" method

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -61,6 +61,7 @@ void task1() {
     printArray(arrB, size);
 
     copyArray(arrA, arrB, size);
+    printf("\n%-12s: %d\t", "check sort", bubbleSortOptimizedCheckSort(arrB, size));
 }
 
 int bubbleSortClassic(int *array, size_t size) {
