b3e11a25766db60070c1905b7775ad166fd0802e
geeksploit <geeksploit@shootan.com>
2017-10-11 18:08:21 +0300

    Iterate through the head of the array discarding the tail

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -71,6 +71,8 @@ int bubbleSortClassic(int *array, size_t size) {
 int bubbleSortOptimizedMinusI(int *array, size_t size) {
     int counter = 0;
     for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+        }
     }
     return counter;
 }
