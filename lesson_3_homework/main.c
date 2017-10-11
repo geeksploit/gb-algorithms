c0f748d2d8afa7367c994d8d21406b27a3fed6e5
geeksploit <geeksploit@shootan.com>
2017-10-11 18:41:51 +0300

    Reset the "sorted" flag

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -161,6 +161,7 @@ int bubbleSortOptimizedTillLastCheckSort(int *array, size_t size) {
             if (array[j] > array[j + 1]) {
                 swap(&array[j], &array[j + 1]);
                 currentSwap = j;
+                isSorted = 0;
             }
         }
     }
