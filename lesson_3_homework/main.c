10f719eb40239d15ad4d7bdac22c21d280630be9
geeksploit <geeksploit@shootan.com>
2017-10-11 18:21:23 +0300

    Copy the source array

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -55,6 +55,8 @@ void task1() {
     copyArray(arrA, arrB, size);
     printf("\n%-12s: %d\t", "minus i", bubbleSortOptimizedMinusI(arrB, size));
     printArray(arrB, size);
+
+    copyArray(arrA, arrB, size);
 }
 
 int bubbleSortClassic(int *array, size_t size) {
