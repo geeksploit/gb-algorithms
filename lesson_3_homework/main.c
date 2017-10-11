0a4c977df3c110ae663c84b7e3128adca2b145a9
geeksploit <geeksploit@shootan.com>
2017-10-11 18:34:25 +0300

    Increment the operations counter

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -133,6 +133,7 @@ int bubbleSortOptimizedMinusICheckSort(int *array, size_t size) {
     for (int i = 0; i < size; i++) {
         int isSorted = 1;
         for (int j = 0; j < size - 1 - i; j++) {
+            counter++;
         }
     }
     return counter;
