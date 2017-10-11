08fd1d9e477d6fe7dafc83475eb829233575e988
geeksploit <geeksploit@shootan.com>
2017-10-11 17:59:22 +0300

    Iterate through the array except the last element

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -53,6 +53,8 @@ int bubbleSortClassic(int *array, size_t size) {
     int counter = 0;
 
     for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - 1; j++) {
+        }
     }
 
     return counter;
