882295a610bb901466f12b4c443af47b9807aa5e
geeksploit <geeksploit@shootan.com>
2017-10-11 18:33:11 +0300

    Define the "-i checked" method stub

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -128,6 +128,11 @@ int bubbleSortOptimizedCheckSort(int *array, size_t size) {
     return counter;
 }
 
+int bubbleSortOptimizedMinusICheckSort(int *array, size_t size) {
+    int counter = 0;
+    return counter;
+}
+
 //<editor-fold desc="Utility functions">
 
 void copyArray(int *arrA, int *arrB, size_t size) {
--- lesson_3_homework/main.h
+++ lesson_3_homework/main.h
@@ -15,6 +15,8 @@ void copyArray(int *arrA, int *arrB, size_t size);
 
 int bubbleSortOptimizedCheckSort(int *array, size_t size);
 
+int bubbleSortOptimizedMinusICheckSort(int *array, size_t size);
+
 void printArray(int *array, size_t size);
 
 void swap(int *a, int *b);
