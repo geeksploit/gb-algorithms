2b58568411a8696fb9741c62297cbe0cf46f34a5
geeksploit <geeksploit@shootan.com>
2017-10-12 23:11:29 +0300

    Add a menu item for the task2

--- lesson_3_homework/main.c
+++ lesson_3_homework/main.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 int main() {
-    void (*menu[])(void) = {&task0, &task1};
+    void (*menu[])(void) = {&task0, &task1, &task2};
     int menuSize = sizeof(menu) / sizeof(menu[0]);
 
     int choice = -1;
@@ -17,6 +17,7 @@ int main() {
         printf("\nplease enter your choice to proceed:");
         printf("\n[%2d] %s", 0, "exit");
         printf("\n[%2d] %s", 1, "bubble sort");
+        printf("\n[%2d] %s", 2, "shaker sort");
         printf("\n> ");
     } while (scanf("%d", &choice));
 
