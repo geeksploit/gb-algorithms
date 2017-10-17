9a29ba9d0e85afc22600eda9d475562e02aba98c
geeksploit <geeksploit@shootan.com>
2017-10-17 23:37:05 +0300

    Add menu stub to version control

---
+++ lesson_5_homework/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int main() {
+    void (*menu[])(void) = {&task_0};
+    int menuSize = sizeof(menu) / sizeof(menu[0]);
+
+    int choice = -1;
+    do {
+        if (choice >= 0 && choice < menuSize) {
+            printf("\nTASK %d\n", choice);
+            menu[choice]();
+        }
+        printf("\n\nMENU");
+        printf("\nplease enter your choice to proceed:");
+        printf("\n[%2d] %s", 0, "exit");
+        printf("\n> ");
+    } while (scanf("%d", &choice));
+
+    return 0;
+}
+
+/*
+ * Exit the application.
+ */
+void task_0() {
+    printf("thanks for staying with us, bye\n");
+    exit(0);
+}
