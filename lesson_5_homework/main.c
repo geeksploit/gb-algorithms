84e3a6d72f1ba2bc2e6793350793e8a36c03308b
geeksploit <geeksploit@shootan.com>
2017-10-18 00:50:27 +0300

    Add a menu item for the task 1

--- lesson_5_homework/main.c
+++ lesson_5_homework/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include "task_1/decimal_to_binary.h"
 
 int main() {
-    void (*menu[])(void) = {&task_0};
+    void (*menu[])(void) = {&task_0, &task_1};
     int menuSize = sizeof(menu) / sizeof(menu[0]);
 
     int choice = -1;
@@ -15,6 +16,7 @@ int main() {
         printf("\n\nMENU");
         printf("\nplease enter your choice to proceed:");
         printf("\n[%2d] %s", 0, "exit");
+        printf("\n[%2d] %s", 1, "decimal to binary using stack");
         printf("\n> ");
     } while (scanf("%d", &choice));
 
