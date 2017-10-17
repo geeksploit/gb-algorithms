fc8d7f905ec375d722cedc2e3ddf062401938f27
geeksploit <geeksploit@shootan.com>
2017-10-18 00:44:49 +0300

    Read a decimal number from the console

--- lesson_5_homework/task_1/decimal_to_binary.c
+++ lesson_5_homework/task_1/decimal_to_binary.c
@@ -2,7 +2,11 @@
  * 1. Реализовать перевод из 10 в 2 систему счисления с использованием стека.
  */
 
+#include <stdio.h>
 #include "decimal_to_binary.h"
 
 void task_1() {
+    int decimal;
+    printf("please enter a decimal to covert: ");
+    scanf("%d", &decimal);
 }
