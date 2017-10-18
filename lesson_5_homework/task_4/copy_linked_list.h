e62496a6381c089973df1fa5f71d646541c863a8
geeksploit <geeksploit@shootan.com>
2017-10-18 15:32:45 +0300

    Define a list node struct and type

--- lesson_5_homework/task_4/copy_linked_list.h
+++ lesson_5_homework/task_4/copy_linked_list.h
@@ -3,4 +3,10 @@
 
 #define ListValue char
 
+struct ListNodeStruct {
+    ListValue value;
+    struct ListNodeStruct nextNode;
+};
+typedef struct ListNodeStruct ListNode;
+
 #endif //LESSON_5_HOMEWORK_COPY_LINKED_LIST_H
