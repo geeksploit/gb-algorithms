4872e0e2d597476ee42d94a4198433f4e21eb863
geeksploit <geeksploit@shootan.com>
2017-10-21 13:10:50 +0300

    Get a string from the console

--- lesson_6_homework/task_1.c
+++ lesson_6_homework/task_1.c
@@ -1,11 +1,17 @@
+#include <stdio.h>
 #include <mem.h>
 
 #define MAX_LINE_LENGTH 255
+#define EVALUATE(x) #x
+#define STRINGIFY(x) EVALUATE(x)
 
 unsigned int hash_sum(char *input_string);
 
 int main() {
     char input_string[MAX_LINE_LENGTH];
+    printf("please enter a string of characters: ");
+    scanf("%" STRINGIFY(MAX_LINE_LENGTH) "s", input_string);
+
     return 0;
 }
 
