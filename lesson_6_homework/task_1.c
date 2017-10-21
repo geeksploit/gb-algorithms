ea0f571389879254b51ee5f36787c77d23a7bb6c
geeksploit <geeksploit@shootan.com>
2017-10-21 13:08:10 +0300

    Iterate through the given string

--- lesson_6_homework/task_1.c
+++ lesson_6_homework/task_1.c
@@ -1,3 +1,5 @@
+#include <mem.h>
+
 unsigned int hash_sum(char *input_string);
 
 int main() {
@@ -6,5 +8,7 @@ int main() {
 
 unsigned int hash_sum(char *input_string) {
     unsigned int hash = 0;
+    for (int i = 0, size = strlen(input_string); i < size; i++) {
+    }
     return hash;
 }
