0f25334d0570f8cc4e5c82985d6cb0a7e251b38e
geeksploit <geeksploit@shootan.com>
2017-10-21 18:01:38 +0300

    Define the action to perform with the nodes

--- lesson_6_homework/task_2.c
+++ lesson_6_homework/task_2.c
@@ -1,6 +1,8 @@
 #include "common/binary_tree.h"
 
 int main() {
+    t_function action = &printStringData;
+
     Node *root = makeNode("test");
     Node *left = appendNode(root, LEFT, makeNode("left_1"));
     Node *right = appendNode(root, RIGHT, makeNode("right_1"));
