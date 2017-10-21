6e9a23d9ab028401df872b713414e561c4dffdcd
geeksploit <geeksploit@shootan.com>
2017-10-21 17:04:21 +0300

    Declare binary tree node structure and type

--- lesson_6_homework/common/binary_tree.h
+++ lesson_6_homework/common/binary_tree.h
@@ -1,4 +1,12 @@
 #ifndef LESSON_6_HOMEWORK_BINARY_TREE_H
 #define LESSON_6_HOMEWORK_BINARY_TREE_H
 
+struct Node {
+    struct Node *parent;
+    struct Node *left;
+    struct Node *right;
+    void *data;
+};
+typedef struct Node Node;
+
 #endif //LESSON_6_HOMEWORK_BINARY_TREE_H
